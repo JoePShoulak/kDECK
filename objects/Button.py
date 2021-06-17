9d72da5dd9f1de40dfb5df87cb035547c5369b5f
JoePShoulak <joepshoulak2@gmail.com>
2021-06-17 13:26:46 -0500

    Changes by bette

--- objects/Button.py
+++ objects/Button.py
@@ -1,19 +1,30 @@
+from abc import abstractmethod, ABC
+
 from PIL import Image
 from StreamDeck.ImageHelpers import PILHelper
 
 
-class Button:
-    def __init__(self, deck, location, name, icon, page=False):
+class Button(ABC):
+    def __init__(self, deck, location, name, icon):
         self.deck = deck
         self.name = name
         self.location = location
         self.icon = icon
-        self.page = page
 
     def display_button(self):
         icon = Image.open(self.icon)
         image = PILHelper.create_scaled_image(self.deck, icon)
         self.deck.set_key_image(self.location, PILHelper.to_native_format(self.deck, image))
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class NavButton(Button):
+    def __init__(self, deck, location, name, icon, page):
+        self.page = page
+        super().__init__(deck, location, name, icon)
 
     def execute(self):
         self.page.render_buttons()
